4e4345088f051b3d6f4c2c468275877497bee1b6
Joshtyn <77408789+jjjzambrano@users.noreply.github.com>
2023-01-10 22:49:11 -0500

    Microservicios

---
+++ api_persona/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class Tipo_persona (models.Model):
+    tipo = models.CharField(max_length=80)  
+
+class Persona (models.Model):
+    nombre = models.CharField(max_length=80)
+    cedula = models.IntegerField()
+    fecha_nacimiento = models.DateField(auto_now=False, auto_now_add=False)
+    monto_primera_compra = models.FloatField()
+    tipo=models.ForeignKey(Tipo_persona,on_delete=models.PROTECT)
