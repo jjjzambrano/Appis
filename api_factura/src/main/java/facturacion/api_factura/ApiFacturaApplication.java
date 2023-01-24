6d25d6296cecd19b7cb49f4ea17b740eb45f0d2d
Joshtyn <77408789+jjjzambrano@users.noreply.github.com>
2023-01-24 02:49:59 -0500

    Reporte

--- api_factura/src/main/java/facturacion/api_factura/ApiFacturaApplication.java
+++ api_factura/src/main/java/facturacion/api_factura/ApiFacturaApplication.java
@@ -2,7 +2,9 @@ package facturacion.api_factura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ApiFacturaApplication {
 
--- api_factura/src/main/java/facturacion/api_factura/factura/FacturaController.java
+++ api_factura/src/main/java/facturacion/api_factura/factura/FacturaController.java
@@ -11,35 +11,61 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("api/factura")
-@CrossOrigin({"*"})
+@CrossOrigin({ "*" })
 public class FacturaController {
-    @Autowired FacturaService facturaService;
+    @Autowired
+    FacturaService facturaService;
 
     @GetMapping("/")
-    public List<Factura> findAll(){
+    public List<Factura> findAll() {
         return facturaService.findAll();
     }
 
     @GetMapping("/{id}/")
-    public Factura findById(@PathVariable Long id){
+    public Factura findById(@PathVariable Long id) {
         return facturaService.findById(id);
     }
 
     @PostMapping("/")
-    public Factura save(@RequestBody Factura entity){
+    public Factura save(@RequestBody Factura entity) {
         return facturaService.save(entity);
     }
 
     @PutMapping("/{id}/")
-    public Factura update(@RequestBody Factura entity){
+    public Factura update(@RequestBody Factura entity) {
         return facturaService.save(entity);
     }
 
     @DeleteMapping("/{id}/")
-    public void deleteById(@PathVariable Long id){
+    public void deleteById(@PathVariable Long id) {
         facturaService.deleteById(id);
     }
+
+    @GetMapping("/pdf/{id}/")
+    public ResponseEntity<byte[]> getFacturaReporte(@PathVariable long id) throws JRException {
+
+        JasperPrint reporte = facturaService.getFacturaReporte(id);
+
+        if (reporte == null)
+            return new ResponseEntity<byte[]>(null, null, HttpStatus.NOT_FOUND);
+
+        HttpHeaders headers = new HttpHeaders();
+        // set the PDF format
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("filename", "factura.pdf");
+        // create the report in PDF format
+        return new ResponseEntity<byte[]>(JasperExportManager.exportReportToPdf(reporte), headers, HttpStatus.OK);
+
+    }
+
 }
