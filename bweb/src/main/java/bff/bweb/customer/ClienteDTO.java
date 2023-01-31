128d0beaf7e8f51b2743b4caeb5d76913f53c982
Joshtyn <77408789+jjjzambrano@users.noreply.github.com>
2023-01-31 03:22:43 -0500

    BFF y Auth Funcionando

---
+++ bweb/src/main/java/bff/bweb/customer/ClienteDTO.java
@@ -0,0 +1,16 @@
+package bff.bweb.customer;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class ClienteDTO {
+    
+    private Long id;
+    private String nombre;
+    private String cedula;
+    private Date fecha_nacimiento;
+    private double monto_primera_compra;
+    private Long tipo;
+}
